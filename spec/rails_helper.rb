56479dbdd68ddb941ff1b43ca7007b40acc80c27
Juliano Gomes <julianobsg@gmail.com>
2021-06-17 20:16:13 -0300

    Add simple VCR config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'vcr'
+
+VCR.config do |c|
+  c.cassette_library_dir     = 'spec/cassettes'
+  c.hook_into :faraday
+  c.filter_sensitive_data('<API_KEY>') { ENV['MARVEL_PUBLIC_KEY'] }
+  c.default_cassette_options = {
+    record: :new_episodes,
+    match_requests_on: [:method,
+     VCR.request_matchers.uri_without_params(:ts, :hash)]
+  }
+end
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
